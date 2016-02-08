db3ac68134dae06463a184679edecebd131a0478
Abigail Rolling and Brad London <arolling@gmail.com>
2016-02-08 15:11:43 -0800

    Write code to pass all specs

--- src/main/java/LeetSpeak.java
+++ src/main/java/LeetSpeak.java
@@ -9,4 +9,29 @@ public class LeetSpeak {
   public static void main(String[] args) {
 
   }
+  public static String leetSpeakTranslate(String phrase){
+
+    phrase = phrase.replace('e' , '3');
+    phrase = phrase.replace('E' , '3');
+    phrase = phrase.replace('o' , '0');
+    phrase = phrase.replace('O' , '0');
+    phrase = phrase.replace('I' , '1');
+    String[] phraseArray = phrase.split(" ");
+    for(Integer index = 0 ; index < phraseArray.length ; index++) {
+      if (phraseArray[index].startsWith("S") || phraseArray[index].startsWith("s")) {
+        phraseArray[index] = phraseArray[index].replace('s' , 'z');
+        phraseArray[index] = phraseArray[index].replace('S' , 'Z');
+        if (phraseArray[index].charAt(0) == 'z') {
+          phraseArray[index] = phraseArray[index].replaceFirst("z", "s");
+        } else {
+          phraseArray[index] = phraseArray[index].replaceFirst("Z", "S");
+        }
+      } else {
+        phraseArray[index] = phraseArray[index].replace('s' , 'z');
+        phraseArray[index] = phraseArray[index].replace('S' , 'Z');
+      }
+    }
+    String newPhrase = String.join(" ", phraseArray);
+    return newPhrase;
+  }
 }
--- src/test/java/LeetSpeakTest.java
+++ src/test/java/LeetSpeakTest.java
@@ -2,8 +2,48 @@ import org.junit.*;
 import static org.junit.Assert.*;
 import java.util.ArrayList;
 
-public class LeetSparkTest {
+public class LeetSpeakTest {
 
+  @Test
+  public void leetSpeakTranslate_returnsAStringAsIsWhenNoLeetspeakIsPresent_wordDoesntChange() {
+    LeetSpeak testApp = new LeetSpeak();
+    assertEquals("happy", testApp.leetSpeakTranslate("happy"));
+  }
 
-  
+  @Test
+  public void leetSpeakTranslate_replacesEveryEInAStringWithA3_wordChanges() {
+    LeetSpeak testApp = new LeetSpeak();
+    assertEquals("3l3phant", testApp.leetSpeakTranslate("Elephant"));
+  }
+
+  @Test
+  public void leetSpeakTranslate_replacesEveryOInAStringWithA0_wordChanges() {
+    LeetSpeak testApp = new LeetSpeak();
+    assertEquals("b00 b00", testApp.leetSpeakTranslate("boo boo"));
+  }
+
+  @Test
+  public void leetSpeakTranslate_replacesEveryIInAStringWithA1_wordChanges() {
+    LeetSpeak testApp = new LeetSpeak();
+    assertEquals("1 lik3 1k3", testApp.leetSpeakTranslate("I like Ike"));
+  }
+
+  @Test
+  public void leetSpeakTranslate_replacesEverySInAStringWithAZ_wordChanges() {
+    LeetSpeak testApp = new LeetSpeak();
+    assertEquals("r0z3z ar3 r3d", testApp.leetSpeakTranslate("roses are red"));
+  }
+
+  @Test
+  public void leetSpeakTranslate_doesNotReplaceSWhenFirstLetter_wordChanges() {
+    LeetSpeak testApp = new LeetSpeak();
+    assertEquals("Suzi3z Sunzhin3", testApp.leetSpeakTranslate("Susies Sunshine"));
+  }
+
+  @Test
+  public void leetSpeakTranslate_makesCorrectReplacementsRegardlessOfCase_ignoresUppercase() {
+    LeetSpeak testApp = new LeetSpeak();
+    assertEquals( "1 Scr3amz y0u scr3amz w3 all scr3am f0r razpb3rry ic3 cr3am!", testApp.leetSpeakTranslate("I Screams you screams we all scream for raspberry ice cream!"));
+  }
+
 }
